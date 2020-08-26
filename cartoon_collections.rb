3ef5ba624f3d5fc6913c1a465cc049f9b0e68bc0
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-26 14:57:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
+  array.find do |cheese|
+    
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
