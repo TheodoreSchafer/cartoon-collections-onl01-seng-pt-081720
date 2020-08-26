b08d980a62f4abe373e3b8ff2c3adb31011857fd
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-26 14:45:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
-def roll_call_dwarves(array)# code an argument here
+def roll_call_dwarves(array)
   index = 0
-  array.each_with_index {|name, index| puts "#{index+1}. #{name} "}# Your code here
+  array.each_with_index {|name, index| puts "#{index+1}. #{name} "}
 end
 
-def summon_captain_planet(array)# code an argument here
+def summon_captain_planet(array)
    elements = []
    array.collect do |calls|
      elements.push "#{calls.capitalize}!"
@@ -11,8 +11,8 @@ def summon_captain_planet(array)# code an argument here
    return elements
 end
 
-def long_planeteer_calls()# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+
 end
 
 def find_the_cheese()# code an argument here
