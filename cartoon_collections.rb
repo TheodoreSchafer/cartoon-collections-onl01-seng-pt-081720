7931b2f27a1a4f24c91402cdfaf831c1b0775922
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-26 14:35:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves(dwarves)# code an argument here
-  # Your code here
+def roll_call_dwarves(array)# code an argument here
+  index = 0
+  array.each_with_index {|name, index| puts "#{index+1}. #{name} "}# Your code here
 end
 
 def summon_captain_planet()# code an argument here
