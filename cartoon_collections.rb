96ff3c8412dd4ea3332c8f0c4279d6e0b88551ea
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-26 14:42:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(array)# code an argument here
    elements = []
    array.collect do |calls|
      elements.push "#{calls.capitalize}!"
-     
+
 end
 
 def long_planeteer_calls()# code an argument here
